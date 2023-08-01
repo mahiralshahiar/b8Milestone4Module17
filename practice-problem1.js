12ed08298a0621ada50aff498c1696f45d6061cc
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-08-02 04:21:10 +0600

    added practice problem one

---
+++ practice-problem1.js
@@ -0,0 +1,12 @@
+var fruits = ['Apple', 'Banana', 'Orange'];
+
+var bananaIndex = fruits.indexOf('Banana');
+
+console.log(bananaIndex)
+
+fruits[1] = 'Mango';
+
+fruits.pop();
+fruits.push('Watermelon');
+
+console.log(fruits)
