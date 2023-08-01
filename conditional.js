a55e9a57f9c2d48014f42dac5d908f4d7fcffb7d
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-08-02 03:03:22 +0600

    tried if else condition

---
+++ conditional.js
@@ -0,0 +1,21 @@
+var iphonePrice = 120000;
+var myBudget = 9300;
+// if iphone price is less than my budget.I will buy the iphone
+if (iphonePrice < myBudget) {
+    console.log("iphone kine futani marbo");
+}
+
+if (iphonePrice > myBudget) {
+    console.log("kanna kore barite cole ashbo");
+}
+
+
+
+var murgirPrice = 500;
+var murgirJonnoBudget = 300;
+
+if (murgirPrice < murgirJonnoBudget) {
+    console.log("murgi diye biriyani khabo ar halim khabo");
+}
+else { console.log("readymate halim khabo") }
+
