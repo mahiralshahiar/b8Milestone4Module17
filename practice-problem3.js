ae33d3666f9ce2b2bf41eabda0fdd3d5080a44d6
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-08-02 05:04:06 +0600

    added practice  problem 3

---
+++ practice-problem3.js
@@ -0,0 +1,25 @@
+// three number 
+
+var numb1 = 13;
+var numb2 = 79;
+var numb3 = 45;
+
+console.log(Math.max(numb1, numb2), "is the Largest number")
+// part one done 
+
+
+// part two starting 
+
+var slide1 = 9;
+
+var slide2 = 8;
+
+var slide3 = 9;
+
+if (slide1 == slide2 || slide2 == slide3 || slide1 == slide3) {
+
+    console.log("yes, It's a Isosceles ")
+}
+else {
+    console.log("no, It's not a Isosceles")
+}
