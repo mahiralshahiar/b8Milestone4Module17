ca7da52a721b9303f64a92c6c9da21a0f39b2cd2
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-08-02 03:56:17 +0600

    tried if else advance things

---
+++ multi-stage-conditions.js
@@ -0,0 +1,19 @@
+var money = 50;
+// eta change kore kore try krote hobe  
+var cocacola = 45;
+var starship = 35;
+var cocoroll = 20;
+
+if (cocacola < money) {
+    console.log('dan dan cocacola khabo');
+}
+else if (starship < money) {
+    console.log('den den startship i khai')
+}
+
+else if (cocoroll < money) {
+    console.log('cocoroll i den')
+}
+else {
+    console.log('khali cha i khabo')
+}
---
+++ nasted-conditions.js
@@ -0,0 +1,19 @@
+var english = false;
+var cgpa4 = true;
+var ict = true;
+
+if (english == true) {
+    if (cgpa4 == true) {
+        if (ict == false)
+            console.log("bah valoi to but ict to parona")
+        else {
+            console.log('bah ict paro mane sob paro')
+        }
+
+    }
+}
+
+
+else {
+    console.log('english i parona baki sob dekhe ki korbo')
+}
