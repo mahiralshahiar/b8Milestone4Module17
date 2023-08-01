ab5477f6d0f0786146ebfe8eea38429fbc9f106f
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-08-02 04:42:56 +0600

    added Practice Problem 2

---
+++ practice-problem2.js
@@ -0,0 +1,159 @@
+// Results grande system 
+
+var mahirNumber = 85;
+var tomNumber = 66;
+var janeNumber = 95;
+var peterNumber = 68;
+var johnNumber = 40;
+
+// Mahir number grade
+
+if (mahirNumber >= 80) {
+    console.log("Mahir is Grade A");
+}
+else {
+
+    if (mahirNumber >= 60) {
+        console.log("Mahir is Grade B");
+    }
+    else {
+        if (mahirNumber >= 50) {
+            console.log("Mahir is Grade C");
+        }
+        else {
+            if (mahirNumber >= 40) {
+                console.log("Mahir is Grade D");
+            }
+            else {
+                if (mahirNumber <= 39) {
+                    console.log("mahri is Grade F");
+                }
+            }
+
+        }
+    }
+}
+
+
+
+// Toms numebr grade
+
+if (tomNumber >= 80) {
+    console.log("Tom is Grade A");
+}
+else {
+
+    if (tomNumber >= 60) {
+        console.log("Tom is Grade B");
+    }
+    else {
+        if (tomNumber >= 50) {
+            console.log("Tom is Grade C");
+        }
+        else {
+            if (tomNumber >= 40) {
+                console.log("Tom is Grade D");
+            }
+            else {
+                if (tomNumber <= 39) {
+                    console.log("Tom is Grade F");
+                }
+            }
+
+        }
+    }
+}
+
+
+// Janes Number
+
+
+if (janeNumber >= 80) {
+    console.log("Jane is Grade A");
+}
+else {
+
+    if (janeNumber >= 60) {
+        console.log("Jane is Grade B");
+    }
+    else {
+        if (janeNumber >= 50) {
+            console.log("Jane is Grade C");
+        }
+        else {
+            if (janeNumber >= 40) {
+                console.log("Jane is Grade D");
+            }
+            else {
+                if (janeNumber <= 39) {
+                    console.log("Jane is Grade F");
+                }
+            }
+
+        }
+    }
+}
+
+
+// Peter number
+
+
+if (peterNumber >= 80) {
+    console.log("Peter is Grade A");
+}
+else {
+
+    if (peterNumber >= 60) {
+        console.log("Peter is Grade B");
+    }
+    else {
+        if (peterNumber >= 50) {
+            console.log("Peter is Grade C");
+        }
+        else {
+            if (peterNumber >= 40) {
+                console.log("Peter is Grade D");
+            }
+            else {
+                if (peterNumber <= 39) {
+                    console.log("Peter is Grade F");
+                }
+            }
+
+        }
+    }
+}
+
+
+
+//Jhon Number 
+
+if (johnNumber >= 80) {
+    console.log("john is Grade A");
+}
+else {
+
+    if (johnNumber >= 60) {
+        console.log("john is Grade B");
+    }
+    else {
+        if (johnNumber >= 50) {
+            console.log("john is Grade C");
+        }
+        else {
+            if (johnNumber >= 40) {
+                console.log("john is Grade D");
+            }
+            else {
+                if (johnNumber <= 39) {
+                    console.log("john is Grade F");
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
