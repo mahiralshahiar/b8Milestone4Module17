f9035fee48f789970658514608315701134226e7
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-08-02 02:50:36 +0600

    tried comparison js

--- comparison.js
+++ comparison.js
@@ -1,2 +1,37 @@
 // console.log(5 < 6)
-console.log(5 > 6)
+
+// console.log(5 > 6)
+
+// console.log(5 == 6)
+
+// console.log(5 != 6)
+
+// console.log(15 != 15)
+
+// console.log(5 <= 6)
+
+// console.log(6 <= 6)
+
+// console.log(16 <= 6)
+
+// console.log(5 >= 6)
+
+// console.log(2 >= 0)
+
+// console.log(2 >= 0)
+
+var num1 = 56;
+var num2 = 60;
+
+// console.log(num1 < num2)
+
+// console.log(num1 <= num2)
+
+// console.log(num1 === num2)
+
+// multiple condition
+
+// money1 > money2 && result1 > result2 && height1 > height2
+
+// money1 > money2 || result1 > result2
+
