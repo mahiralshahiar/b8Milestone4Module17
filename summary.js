9eda4c386a66a43780a035a9e1881a3b7fd42d9f
mahiralshahiar <mrmahiralshariar9@gmail.com>
2023-08-02 04:08:21 +0600

    tried sammary

---
+++ summary.js
@@ -0,0 +1,26 @@
+var destination = ['sajek', 'coxbazar', 'nepal ', 'bali', 'SP']
+destination.pop();
+destination[2] = 'thailand';
+destination.push('bangladesh');
+
+console.log(destination[2])
+
+var index = destination.indexOf('bali')
+console.log(index)
+
+
+console.log(destination)
+
+
+//  conditionals
+
+// <, >, ==, !=, <=, >=
+// && ||
+
+if (destination.length > 10) {
+    console.log('bah valoi to ')
+
+}
+else {
+    console.log('na 10 er kom i ache')
+}
